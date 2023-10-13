5abec7743ae8694b1d46d12c48ca58871104ec91
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-10-13 11:10:12 +0600

    design workdetails page

--- app/works/[id]/page.js
+++ app/works/[id]/page.js
@@ -1,4 +1,6 @@
 "use client";
+import {motion} from "framer-motion"
+import { textVariant2 } from "@/utils/motion/motion";
 import React, { useEffect, useState } from "react";
 
 const page = ({ params }) => {
@@ -32,7 +34,7 @@ useEffect(() => {
     <div className="">
       <div className=" w-full relative h-[500px] sm:h-[100vh] bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: `url(${workId?.image})` }}
-      ><div className="bg-[#000] absolute top-0 left-0 px-4 sm:px-16 opacity-[0.5] w-full h-full">
+      ><div className="bg-[#000] absolute top-0 left-0 px-4 sm:px-16 opacity-[0.7] w-full h-full">
     
           
         </div>
@@ -44,7 +46,13 @@ useEffect(() => {
 <p  className="text-[#f6f5f5] text-center capitalize text-[16px] sm:text-[24px] font-semibold tracking-wide font-popping">{ workId?.details}</p>
           </div>
         </div>
-        <div className="absolute px-4 top-0 left-0 w-full h-full flex justify-center items-end">          <img className="mx-auto w-full sm:w-[80%]" src="/images/role.png" alt="" />
+        <div className="absolute px-4 top-0 left-0 w-full h-full flex justify-center items-end">        
+          
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-3 w-full text-center py-10">
+            <p className="text-[15px] text-[#37a5eb] font-bold font-popping"> ROLE <span className="px-2 text-white">FRONT-END DEVELOPER</span> </p>
+            <p className="text-[15px] text-[#37a5eb] font-bold font-popping"> CONTEXT  <span className="px-2 text-white">WEBSITE REDESIGN</span> </p>
+            <p className="text-[15px] text-[#37a5eb] font-bold font-popping"> PERIOD   <span className="px-2 text-white">MIDDLE 2020</span> </p>
+          </div>
 </div>
         
      
@@ -54,7 +62,7 @@ useEffect(() => {
         <div className="">
         <h1 className="text-white text-center text-[25px]  capitalize sm:text-[60px] font-extrabold font-sofia">The Project</h1>
           <div className="mx-auto sm:w-[650px]">
-          <p  className="text-[#f6f5f5]  capitalize text-[16px] sm:text-[18px]  tracking-wide font-popping">{ filter[0]?.details}</p>
+          <p  className="text-[#f6f5f5]  capitalize text-center sm:text-start text-[14px] sm:text-[18px]  tracking-wide font-popping">{ filter[0]?.details}</p>
           <div className="py-8 text-center">
               <a target="_blank" href={filter[0]?.ref}>
               <div
@@ -94,7 +102,55 @@ useEffect(() => {
 </div>
          </div>
       </div>
-      {/*  */}
+      {/*  figma & analysis*/}
+
+      <div className="px-4 bg-white py-20 sm:px-16 mx-auto">
+        <div className="">
+        <motion.h3
+        variants={textVariant2}
+        initial="hidden"
+        whileInView="show"
+        className="text-center text-[#b29487] text-[16px]  font-popping font-semibold tracking-widest	 leading-[30px]"
+      >
+        {" "}
+        ANALYSIS & PREPARATION
+      </motion.h3>
+      <motion.h2
+        variants={textVariant2}
+        initial="hidden"
+        whileInView="show"
+        className="text-center text-[30px] sm:text-[40px] font-extrabold font-sofia py-3  text-[#4a4a4a]"
+      >
+            Branding      </motion.h2>
+          
+          <div className="mx-auto py-4 sm:flex  justify-center  gap-10">
+            <div className="">
+            <motion.h2
+        variants={textVariant2}
+        initial="hidden"
+        whileInView="show"
+        className="text-center sm:text-start text-[25px] sm:text-[40px] font-extrabold font-sofia py-3  text-[#121212]"
+      >
+            An elegant design.
+              </motion.h2>
+              <div className="w-[100px]  bg-[#37a5eb] rounded-md h-[5px]"></div>
+              <motion.h3
+        variants={textVariant2}
+        initial="hidden"
+        whileInView="show"
+        className="text-center mt-5 sm:text-start sm:w-[450px] text-[#121212] text-[16px]  font-popping font-semibold tracking-wider	 leading-[30px]"
+      >
+        {" "}
+        As the  <span className="text-[#37a5eb]">Front-end Developer</span>, I was responsible of building the entire UI for the new website, redefining the User Experience and studying new interactions between the User and the Interface.
+
+One of the most exciting experience was integrating the entire front-end system with the <span className="text-[#37a5eb]"> Ruby on Rails Application </span> and the change page animation.
+      </motion.h3>
+            </div>
+            <div className="pt-6 sm:pt-0"><img src={filter[0]?.figma} alt="" /></div>
+          </div>
+        </div>
+       </div>
+      {/* nextg-project */}
     </div>
   );
 };
